ba328a62836b0a926c5fde1e93c2199d92988b3b
Prateek <prateekvasanthpai@gmail.com>
2022-07-30 10:50:38 +0530

    Get Random Single Digit Number

--- SequencePracticeProblem.js
+++ SequencePracticeProblem.js
@@ -1,2 +1,6 @@
 console.log("Welcome to Sequence Practice Problem")
 
+//Random Function Math.floor(Math.random() * 10); to get Single Digit.
+let singleDigitRandomNumber = Math.floor(Math.random() * 10);  
+console.log("Random single digit : "+singleDigitRandomNumber);
+
