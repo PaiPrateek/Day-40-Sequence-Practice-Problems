32ad449add3b88fd77368664e2e2ce23675f3607
Prateek <prateekvasanthpai@gmail.com>
2022-07-30 10:57:19 +0530

    Read 5 random 2 digit number and therir sum and average

--- SequencePracticeProblem.js
+++ SequencePracticeProblem.js
@@ -15,3 +15,15 @@ let RandomNumber1 = Math.floor(Math.random() * 6)+1;
 let RandomNumber2 = Math.floor(Math.random() * 6)+1;  
 console.log("Addition of 2 random dice numbers : "+(RandomNumber1+RandomNumber2));
 
+//Calculate Sum and Average of 5 Random 2 Digit Number
+let num = 0;
+let sum = 0;
+let countOfRandommNumbers = 5;
+
+for(let i = 0; i < countOfRandommNumbers; i++){
+    num = Math.floor(Math.random()*100);
+    sum = sum + num;
+}
+let average = sum / countOfRandommNumbers;
+console.log("Sum : " +sum +", Average : "+average);
+
