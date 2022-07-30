5299dab455f3fc19c4987bf7ec7e377430fcf645
Prateek <prateekvasanthpai@gmail.com>
2022-07-30 10:54:26 +0530

    Adding 2 random dice number

--- SequencePracticeProblem.js
+++ SequencePracticeProblem.js
@@ -8,3 +8,10 @@ console.log("Random single digit : "+singleDigitRandomNumber);
 //use Random to get Dice Number between 1 to 6
 let RandomNumber = Math.floor(Math.random() * 6)+1;  
 console.log("Random Dice Number : "+RandomNumber);
+
+
+//use Random to get Dice Number between 1 to 6
+let RandomNumber1 = Math.floor(Math.random() * 6)+1;  
+let RandomNumber2 = Math.floor(Math.random() * 6)+1;  
+console.log("Addition of 2 random dice numbers : "+(RandomNumber1+RandomNumber2));
+
